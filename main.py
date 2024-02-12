1807b16a089353c888afa0830620d507d4aa1035
3rickDJ <erick@makingdevs.com>
2024-02-12 13:19:24 -0600

    refactor naming to spansih jeje

--- main.py
+++ main.py
@@ -6,181 +6,97 @@ def main():
     main function
     """
 
-    starting_node = [[0, 0]]
-    jugs = [3,4]
-    goal_amount = 2
-    check_dict = {}
-    is_depth = False
-    search(starting_node, jugs, goal_amount, check_dict, is_depth)
+    nodo_inicial = [[0, 0]]
+    jarras = [3,4]
+    objetivo = 2
+    visitados = {}
+    enProfundidad = False
+    busqueda(nodo_inicial, jarras, objetivo, visitados, enProfundidad)
 
-def get_index(node):
+def obtenerIndice(node):
     # la llave unica de un estado para ponerla en el diccinario y que no colisiones
     return pow(7, node[0]) * pow(5, node[1])
 
-def is_goal(path, goal_amount):
-    """
-    Returns True, if the given path terminates at the goal node.
+def esObjetivo(camino, objetivo):
+    return camino[-1][1] == objetivo 
 
-    path: a list of nodes representing the path to be checked
-    goal_amount: an integer representing the desired amount of water
-    """
+def esRevisado(node, visitado):
+    # revisar si ya añadimos ese nodo a la frontera
+    # si fue visitado returnar el valor del nodo, True, de otra forma False
+    return visitado.get(obtenerIndice(node), False)
 
-    print("Checking if the gaol is achieved...")
+def siguientes(jarras, camino, visitado):
+    resultado = []
+    siguientes_nodos = []
+    nodo = []
 
-    return path[-1][1] == goal_amount 
+    primera_jarra_max = jarras[0]
+    segunda_jarra_max = jarras[1]
 
-def been_there(node, check_dict):
-    """
-    Returns True, if the given node is already visited
+    a = camino[-1][0]  # cantidad inicial de la primera jarra
+    b = camino[-1][1]  # cantidad inicial de la segunda jarra
 
-    node: a list of two integers representing current state of the jugs
-    check_dict: a dictionary storing visited nodes
-    """
+    # 1. llena la primera jarra
+    nodo.append(primera_jarra_max)
+    nodo.append(b)
+    if not esRevisado(nodo, visitado):
+        siguientes_nodos.append(nodo)
+    nodo = []
 
-    print("Checking if {0} is visited before...".format(node))
+    # 2. llena la segunda jarra
+    nodo.append(a)
+    nodo.append(segunda_jarra_max)
+    if not esRevisado(nodo, visitado):
+        siguientes_nodos.append(nodo)
+    nodo = []
 
-    return check_dict.get(get_index(node), False)
+    # 3. vertir la segunda jarra dentro de la primera
+    nodo.append(min(primera_jarra_max, a + b))
+    nodo.append(b - (nodo[0] - a))  # b - ( a' - a)
+    if not esRevisado(nodo, visitado):
+        siguientes_nodos.append(nodo)
+    nodo = []
 
-def next_transitions(jugs, path, check_dict):
-    """
-    Returns list of all possible transitions whcih do not cause loops
+    # 4. vertir la primera jarra dentro de la segunda
+    nodo.append(min(a + b, segunda_jarra_max))
+    nodo.insert(0, a - (nodo[0] - b))
+    if not esRevisado(nodo, visitado):
+        siguientes_nodos.append(nodo)
+    nodo = []
 
-    jugs: a list of two integers representing volumes of the jugs
-    path: a list of nodes represeting the current path
-    check_dict: a dictionary storing visited nodes
-    """
+    # 5. primera jarra vacía
+    nodo.append(0)
+    nodo.append(b)
+    if not esRevisado(nodo, visitado):
+        siguientes_nodos.append(nodo)
+    nodo = []
 
-    print("Finding next transitions and checking for the loops...")
+    # 6. segunda jarra vacía
+    nodo.append(a)
+    nodo.append(0)
+    if not esRevisado(nodo, visitado):
+        siguientes_nodos.append(nodo)
 
-    result = []
-    next_nodes = []
-    node = []
+    # lista de los siguiente nodos
+    for i in range(0, len(siguientes_nodos)):
+        temp = list(camino)
+        temp.append(siguientes_nodos[i])
+        resultado.append(temp)
 
-    a_max = jugs[0]
-    b_max = jugs[1]
-
-    a = path[-1][0]  # initial amount in the first jug
-    b = path[-1][1]  # initial amount in the second jug
-
-    # 1. fill in the first jug
-    node.append(a_max)
-    node.append(b)
-    if not been_there(node, check_dict):
-        next_nodes.append(node)
-    node = []
-
-    # 2. fill in the second jug
-    node.append(a)
-    node.append(b_max)
-    if not been_there(node, check_dict):
-        next_nodes.append(node)
-    node = []
-
-    # 3. second jug to first jug
-    node.append(min(a_max, a + b))
-    node.append(b - (node[0] - a))  # b - ( a' - a)
-    if not been_there(node, check_dict):
-        next_nodes.append(node)
-    node = []
-
-    # 4. first jug to second jug
-    node.append(min(a + b, b_max))
-    node.insert(0, a - (node[0] - b))
-    if not been_there(node, check_dict):
-        next_nodes.append(node)
-    node = []
-
-    # 5. empty first jug
-    node.append(0)
-    node.append(b)
-    if not been_there(node, check_dict):
-        next_nodes.append(node)
-    node = []
-
-    # 6. empty second jug
-    node.append(a)
-    node.append(0)
-    if not been_there(node, check_dict):
-        next_nodes.append(node)
-
-    # create a list of next paths
-    for i in range(0, len(next_nodes)):
-        temp = list(path)
-        temp.append(next_nodes[i])
-        result.append(temp)
-
-    if len(next_nodes) == 0:
-        print("No more unvisited nodes...\nBacktracking...")
+    if len(siguientes_nodos) == 0:
+        #ya no hay mas nodos por generar
+        # ya sea por que no son validos o ya han sido visitados
+        pass
     else:
-        print("Possible transitions: ")
-        for nnode in next_nodes:
-            print(nnode)
-
-    return result
+        # print("Movimientos válidos")
+        print(siguientes_nodos)
+    return resultado
 
 
-def transition(old, new, jugs):
-    """
-    returns a string explaining the transition from old state/node to new state/node
-
-    old: a list representing old state/node
-    new: a list representing new state/node
-    jugs: a list of two integers representing volumes of the jugs
-    """
-
-    a = old[0]
-    b = old[1]
-    a_prime = new[0]
-    b_prime = new[1]
-    a_max = jugs[0]
-    b_max = jugs[1]
-
-    if a > a_prime:
-        if b == b_prime:
-            return "Clear {0}-liter jug:\t\t\t".format(a_max)
-        else:
-            return "Pour {0}-liter jug into {1}-liter jug:\t".format(a_max, b_max)
-    else:
-        if b > b_prime:
-            if a == a_prime:
-                return "Clear {0}-liter jug:\t\t\t".format(b_max)
-            else:
-                return "Pour {0}-liter jug into {1}-liter jug:\t".format(b_max, a_max)
-        else:
-            if a == a_prime:
-                return "Fill {0}-liter jug:\t\t\t".format(b_max)
-            else:
-                return "Fill {0}-liter jug:\t\t\t".format(a_max)
 
 
-def print_path(path, jugs):
-    """
-    prints the goal path
 
-    path: a list of nodes representing the goal path
-    jugs: a list of two integers representing volumes of the jugs
-    """
-
-    print("Starting from:\t\t\t\t", path[0])
-    for i in  range(0, len(path) - 1):
-        print(i+1,":", transition(path[i], path[i+1], jugs), path[i+1])
-
-def search(starting_node, jugs, goal_amount, check_dict, is_depth):
-    """
-    searchs for a path between starting node and goal node
-
-    starting_node: a list of list of two integers representing initial state of the jugs
-    jugs: a list of two integers representing volumes of the jugs
-    goal_amount: an integer represting the desired amount
-    check_dict: a dictionary storing visited nodes
-    is_depth: implements DFS, if True; BFS otherwise
-    """
-
-    if is_depth:
-        print("Implementing DFS...")
-    else:
-        print("Implementing BFS...")
-
+def busqueda(starting_node, jugs, goal_amount, check_dict, is_depth):
     goal = []
     accomplished = False
 
@@ -189,26 +105,21 @@ def search(starting_node, jugs, goal_amount, check_dict, is_depth):
 
     while len(q) != 0:
         path = q.popleft()
-        check_dict[get_index(path[-1])] = True
-        if len(path) >= 2:
-            print(transition(path[-2], path[-1], jugs), path[-1])
-        if is_goal(path, goal_amount):
+        check_dict[obtenerIndice(path[-1])] = True
+        if esObjetivo(path, goal_amount):
             accomplished = True
             goal = path
             break
 
-        next_moves = next_transitions(jugs, path, check_dict)
+        next_moves = siguientes(jugs, path, check_dict)
         for i in next_moves:
-            if is_depth:
-                q.appendleft(i)
-            else:
-                q.append(i)
+            q.append(i)
 
     if accomplished:
-        print("The goal is achieved\nPrinting the sequence of the moves...\n")
-        print_path(goal, jugs)
+        print("Solución 🔥😍🥑👌🏼")
+        print(goal)
     else:
-        print("Problem cannot be solved.")
+        print("No se encontró una solución 😭")
 
 
 if __name__ == '__main__':
